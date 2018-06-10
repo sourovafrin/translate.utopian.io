4f411833df7ebda291f03c23d05d087d9f8e4b0c
Diego Pucci <me@diegopucci.com>
2018-06-10 16:50:20 +0200

    first commit

---
+++ src/client/components/Story/Body.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import classNames from 'classnames';
+import sanitizeHtml from 'sanitize-html';
+import showdown from 'showdown';
+import embedjs from 'embedjs';
+import { jsonParse } from '../../helpers/formatter';
+import sanitizeConfig from '../../vendor/SanitizeConfig';
+import { imageRegex, dtubeImageRegex, rewriteRegex } from '../../helpers/regexHelpers';
+import htmlReady from '../../vendor/steemitHtmlReady';
+import improve from '../../helpers/improve';
+import PostFeedEmbed from './PostFeedEmbed';
+import './Body.less';
+
+export const converter = new showdown.Converter({
+  tables: true,
+  strikethrough: true,
+});
+
+const getEmbed = link => {
+  const embed = embedjs.get(link, { width: '100%', height: 400, autoplay: false });
+
+  if (_.isUndefined(embed)) {
+    return {
+      provider_name: '',
+      thumbnail: '',
+      embed: link,
+    };
+  }
+
+  return embed;
+};
+
+// Should return text(html) if returnType is text
+// Should return Object(React Compatible) if returnType is Object
+export function getHtml(body, jsonMetadata = {}, returnType = 'Object', options = {}) {
+  const parsedJsonMetadata = jsonParse(jsonMetadata) || {};
+  parsedJsonMetadata.image = parsedJsonMetadata.image || [];
+
+  let parsedBody = body.replace(/<!--([\s\S]+?)(-->|$)/g, '(html comment removed: $1)');
+
+  parsedBody = parsedBody.replace(/^\s+</gm, '<');
+
+  parsedBody.replace(imageRegex, img => {
+    if (_.filter(parsedJsonMetadata.image, i => i.indexOf(img) !== -1).length === 0) {
+      parsedJsonMetadata.image.push(img);
+    }
+  });
+
+  parsedBody = improve(parsedBody);
+  parsedBody = converter.makeHtml(parsedBody);
+
+  const htmlReadyOptions = { mutate: true, resolveIframe: returnType === 'text' };
+  parsedBody = htmlReady(parsedBody, htmlReadyOptions).html;
+  parsedBody = parsedBody.replace(dtubeImageRegex, '');
+
+  if (options.rewriteLinks) {
+    parsedBody = parsedBody.replace(rewriteRegex, (match, p1) => `"${p1 || '/'}"`);
+  }
+
+  parsedBody = sanitizeHtml(
+    parsedBody,
+    sanitizeConfig({
+      secureLinks: options.secureLinks,
+    }),
+  );
+  if (returnType === 'text') {
+    return parsedBody;
+  }
+
+  const sections = [];
+
+  const splittedBody = parsedBody.split('~~~ embed:');
+  for (let i = 0; i < splittedBody.length; i += 1) {
+    let section = splittedBody[i];
+
+    const match = section.match(/^([A-Za-z0-9_-]+) ([A-Za-z]+) (\S+) ~~~/);
+    if (match && match.length >= 4) {
+      const id = match[1];
+      const type = match[2];
+      const link = match[3];
+      const embed = getEmbed(link);
+      sections.push(
+        ReactDOMServer.renderToString(<PostFeedEmbed key={`embed-a-${i}`} inPost embed={embed} />),
+      );
+      section = section.substring(`${id} ${type} ${link} ~~~`.length);
+    }
+    if (section !== '') {
+      sections.push(section);
+    }
+  }
+  // eslint-disable-next-line react/no-danger
+  return <div dangerouslySetInnerHTML={{ __html: sections.join('') }} />;
+}
+
+const Body = props => {
+  const options = {
+    rewriteLinks: props.rewriteLinks,
+    secureLinks: true,
+  };
+  const htmlSections = getHtml(props.body, props.jsonMetadata, 'Object', options);
+  return <div className={classNames('Body', { 'Body--full': props.full })}>{htmlSections}</div>;
+};
+
+Body.propTypes = {
+  body: PropTypes.string,
+  jsonMetadata: PropTypes.string,
+  full: PropTypes.bool,
+  rewriteLinks: PropTypes.bool,
+};
+
+Body.defaultProps = {
+  body: '',
+  jsonMetadata: '',
+  full: false,
+  rewriteLinks: false,
+};
+
+export default Body;
---
+++ src/client/components/Story/BodyShort.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ellipsis from 'text-ellipsis';
+import striptags from 'striptags';
+import { converter } from './Body';
+
+function decodeEntities(body) {
+  return body.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+}
+
+const BodyShort = props => {
+  let body = striptags(converter.makeHtml(striptags(decodeEntities(props.body))));
+  body = body.replace(/(?:https?|ftp):\/\/[\S]+/g, '');
+
+  // If body consists of whitespace characters only skip it.
+  if (!body.replace(/\s/g, '').length) {
+    return null;
+  }
+
+  /* eslint-disable react/no-danger */
+  return (
+    <div
+      className={props.className}
+      dangerouslySetInnerHTML={{ __html: ellipsis(body, props.length, { ellipsis: '…' }) }}
+    />
+  );
+  /* eslint-enable react/no-danger */
+};
+
+BodyShort.propTypes = {
+  className: PropTypes.string,
+  body: PropTypes.string,
+  length: PropTypes.number,
+};
+
+BodyShort.defaultProps = {
+  className: '',
+  body: '',
+  length: 140,
+};
+
+export default BodyShort;
